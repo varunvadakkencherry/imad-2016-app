bdb3ebe1d29dae45527cf31445468996fd03af98
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-10 15:22:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text")
-element.innerHTML ="New HTML"
+var element = document.getElementById("main-text");
+element.innerHTML ="New HTML";
+//move image
+var img = document.getElementById('img');
+img.onClick = function(){
+    alert("Hi");
+};
