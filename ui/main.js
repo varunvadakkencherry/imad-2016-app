56c032c47ea46bd0bad4bb3182407b8954475ed0
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-10 15:32:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML ="New HTML";
 //move image
-var img = document.getElementById("modi");
+var img = document.getElementById("madi");
 img.onClick = function(){
-    img.style.marginLeft = '100px';
+    img.style.marginLeft = "100px";
 };
