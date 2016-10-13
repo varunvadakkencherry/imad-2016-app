ad9cea95b5b0065667ec34946d9a771bf2480798
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-13 14:46:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,4 @@
-//Counter code
-var button = document.GetElementById('counter');
-var counter = 0;
-button.onClick = function (){
+var btn = document.getElementById('counter');
+btn.onClick = function(){
     alert("Test");
-   //// counter = counter + 1;
-  //  var span = document.getElementById('count');
-   // span.innerHTML = counter.toString();
 };
