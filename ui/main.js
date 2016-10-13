86cd03b3416bf9ef378ad389ad8c0cbcd85fc457
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-13 15:03:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 counter = 0;
 function myFunction() {
-    alert("I am an alert box!");
     counter = counter +1;
     var spn = document.getElementById('count');
     spn.innerHTML = counter.toString();
