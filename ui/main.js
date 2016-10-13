4855493dffae0b0ff5b08d2adfb3d243474037a5
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-13 19:21:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,17 @@ function myFunction() {
    request.open('GET', 'http://varunvadakkencherry.imad.hasura-app.io/counter',true );
    request.send(null);
 }
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onClick = function(){
+    var names = ['Name1', 'Name2, 'Name3'];
+    var list = '';
+    for (var i = 0; i < names.length; i++)
+    {
+        list += '<li>' + names[i]+ '</li>'
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
