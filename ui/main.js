1ff60733bf61688c85c40e1456baca5a2f27aea7
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-13 15:19:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,20 @@
-counter = 0;
 function myFunction() {
-    counter = counter +1;
-    var spn = document.getElementById('count');
-    spn.innerHTML = counter.toString();
-}
+   // counter = counter +1;
+   // var spn = document.getElementById('count');
+   // spn.innerHTML = counter.toString();
+   
+   // Create a request
+   var rqst = new XMLHttpRequest();
+   rqst.onreadystatechange = function(){
+       if (rqst.readyState === XMLHttpRequest.DONE)
+       {
+            if(rqst.status === 200){
+                var counter = request.responseText;
+            };
+       };
+   };
+   
+   // Make a request
+   rqst.open('GET', 'http://varunvadakkencherry.imad.hasura-app.io/counter',true );
+   rqst.send(null);
+};
