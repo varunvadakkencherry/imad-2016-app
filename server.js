e2f038261eb4312b58b72e9d74b20090c181ae9c
varunvadakkencherry <varunvadakkencherry@gmail.com>
2016-10-13 19:57:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res){
+    var name = req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+};
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
